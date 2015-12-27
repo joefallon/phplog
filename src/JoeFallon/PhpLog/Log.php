087c86001a4227009c67e9a000a9c99804d2f8c3
Joe Fallon <joseph.t.fallon@gmail.com>
2015-12-27 14:47:17 -0800

    Composer update. Code cleanup was completed. Additional tests were added.

--- src/JoeFallon/PhpLog/Log.php
+++ src/JoeFallon/PhpLog/Log.php
@@ -4,11 +4,6 @@ namespace JoeFallon\PhpLog;
 use Exception;
 use Psr\Log\LoggerInterface;
 
-/**
- * @author    Joseph Fallon <joseph.t.fallon@gmail.com>
- * @copyright Copyright 2014 Joseph Fallon (All rights reserved)
- * @license   MIT
- */
 class Log implements LoggerInterface
 {
     const DEBUG     = 1; // Most Verbose
@@ -36,16 +31,16 @@ class Log implements LoggerInterface
 
 
     /**
-     * @param string $filePath  File path to log
-     * @param int    $level     Priority
+     * @param string $filePath File path to log
+     * @param int    $level    Priority
      *
      * @throws Exception
      */
     public function __construct($filePath, $level)
     {
-        $this->_status     = self::LOG_CLOSED;
-        $this->_dateForm   = 'Y-m-d H:i:s';
-        $this->_level      = $level;
+        $this->_status = self::LOG_CLOSED;
+        $this->_dateForm = 'Y-m-d H:i:s';
+        $this->_level = $level;
         $this->_fileHandle = null;
 
         if($level == self::OFF)
@@ -62,15 +57,11 @@ class Log implements LoggerInterface
         else
         {
             $this->_status = self::OPEN_FAILED;
-            $msg           = "The file could not be opened. Check permissions.";
+            $msg = "The file could not be opened. Check permissions.";
             throw new Exception($msg);
         }
     }
 
-
-    /**
-     * __destruct
-     */
     public function __destruct()
     {
         if($this->_fileHandle == true)
@@ -95,116 +86,6 @@ class Log implements LoggerInterface
         $this->log(self::EMERGENCY, $message, $context);
     }
 
-
-    /**
-     * Action must be taken immediately.
-     *
-     * Example: Entire website down, database unavailable, etc. This should
-     * trigger the SMS alerts and wake you up.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function alert($message, array $context = array())
-    {
-        $this->log(self::ALERT, $message, $context);
-    }
-
-
-    /**
-     * Critical conditions.
-     *
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function critical($message, array $context = array())
-    {
-        $this->log(self::CRITICAL, $message, $context);
-    }
-
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function error($message, array $context = array())
-    {
-        $this->log(self::ERROR, $message, $context);
-    }
-
-
-    /**
-     * Exceptional occurrences that are not errors.
-     *
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things
-     * that are not necessarily wrong.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function warning($message, array $context = array())
-    {
-        $this->log(self::WARN, $message, $context);
-    }
-
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function notice($message, array $context = array())
-    {
-        $this->log(self::NOTICE, $message, $context);
-    }
-
-
-    /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function info($message, array $context = array())
-    {
-        $this->log(self::INFO, $message, $context);
-    }
-
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return null
-     */
-    public function debug($message, array $context = array())
-    {
-        $this->log(self::DEBUG, $message, $context);
-    }
-
-
     /**
      * Logs with an arbitrary level.
      *
@@ -219,7 +100,7 @@ class Log implements LoggerInterface
         if($this->_level <= $level)
         {
             $linePrefix = $this->getLinePrefix($level);
-            $message    = strval($message);
+            $message = strval($message);
             $strContext = '';
 
             if($context != null && count($context) > 0)
@@ -230,37 +111,6 @@ class Log implements LoggerInterface
             $this->writeLine($linePrefix . $message . ' ' . $strContext . "\n");
         }
     }
-
-
-    /**
-     * writeLine
-     *
-     * @param string $line
-     *
-     * @throws Exception
-     */
-    private function writeLine($line)
-    {
-        if($this->_level == self::OFF)
-        {
-            return;
-        }
-
-        if($this->_status != self::LOG_OPEN)
-        {
-            $msg = 'The log file is not open. '
-                   . 'Please check that appropriate permissions have been set.';
-            throw new Exception($msg);
-        }
-
-        if(fwrite($this->_fileHandle, $line) === false)
-        {
-            $msg = 'The log file could not be written to. '
-                   . 'Please check that appropriate permissions have been set.';
-            throw new Exception($msg);
-        }
-    }
-
 
     /**
      * getLinePrefix
@@ -300,5 +150,136 @@ class Log implements LoggerInterface
                 return $time . ' [LOG] ';
                 break;
         }
+    }
+
+    /**
+     * writeLine
+     *
+     * @param string $line
+     *
+     * @throws Exception
+     */
+    private function writeLine($line)
+    {
+        if($this->_level == self::OFF)
+        {
+            return;
+        }
+
+        if($this->_status != self::LOG_OPEN)
+        {
+            $msg = 'The log file is not open. '
+                . 'Please check that appropriate permissions have been set.';
+            throw new Exception($msg);
+        }
+
+        if(fwrite($this->_fileHandle, $line) === false)
+        {
+            $msg = 'The log file could not be written to. '
+                . 'Please check that appropriate permissions have been set.';
+            throw new Exception($msg);
+        }
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->log(self::ALERT, $message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->log(self::CRITICAL, $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function error($message, array $context = array())
+    {
+        $this->log(self::ERROR, $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->log(self::WARN, $message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->log(self::NOTICE, $message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function info($message, array $context = array())
+    {
+        $this->log(self::INFO, $message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->log(self::DEBUG, $message, $context);
     }
 }
--- tests/JoeFallon/PhpLog/LogTests.php
+++ tests/JoeFallon/PhpLog/LogTests.php
@@ -9,7 +9,6 @@ class LogTests extends UnitTest
     /** @var  string */
     private $_logPath;
 
-
     protected function setUp()
     {
         $this->_logPath = BASE_PATH . '/tests/logs/' . date('Y-m-d') . '.log';
@@ -67,5 +66,59 @@ class LogTests extends UnitTest
     {
         $logger = new Log($this->_logPath, Log::DEBUG);
         $logger->alert('Test off message.', array('contextKey' => 'contextVal'));
+    }
+
+    public function test_debug_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->debug('Test debug.');
+    }
+
+    public function test_info_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->info('Test info.');
+    }
+
+    public function test_notice_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->notice('Test notice.');
+    }
+
+    public function test_warning_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->warning('Test warning.');
+    }
+
+    public function test_error_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->error('Test error.');
+    }
+
+    public function test_critical_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->critical('Test critical.');
+    }
+
+    public function test_alert_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->alert('Test alert.');
+    }
+
+    public function test_emergency_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->emergency('Test emergency.');
+    }
+
+    public function test_off_level_without_context()
+    {
+        $logger = new Log($this->_logPath, Log::DEBUG);
+        $logger->alert('Test off message.');
     }
 }
